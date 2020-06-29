408477efcaa1069efe7449f17b218e96aa5d27ae
Psykka <viniciusfabrisilk@gmai.com>
2020-06-29 17:19:49 -0300

    Commando de falar

---
+++ src/commands/fun/Say.js
@@ -0,0 +1,18 @@
+const Command = require('../../lib/strucutures/Command')
+
+module.exports = class Say extends Command {
+  constructor(client){
+    super(client) // inicia a classes contrutora
+    this.name = 'say' // nome do comando
+    this.aliases = ['falar'] // apelido | sinonimo do comando
+    this.category = 'fun' // categoria do comando
+  }
+
+  run({ args, channel }){
+    if(!args[0]) return channel.send('Você precisa escrever algo antes!')
+
+    const mensagem = args.join(' ')
+
+    channel.send(mensagem)
+  }
+}
