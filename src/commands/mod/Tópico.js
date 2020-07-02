9e46927f1ed723cb5078aea9543a964035af6526
Thay-3x <thay>
2020-07-02 10:33:05 -0300

    Comando adicionado Tópico.js

---
+++ src/commands/mod/Tópico.js
@@ -0,0 +1,32 @@
+const Command = require('../../lib/strucutures/Command')
+
+module.exports = class Tópico extends Command {
+  constructor(client) {
+    super(client)
+    this.name = 'tópico'
+    this.aliases = ['msg do tópico']
+    this.category = 'mod'
+  }
+  run({ client, message, args }) {
+    message.delete()
+    if (!message.member.hasPermission('MANAGE_CHANNELS', false, true, true)) {
+      message.reply('**Você não possui permissões para utilizar esse comando!** <a:atencao:723878171216314448>')
+      return 0
+    }
+    if (!message.guild.me.hasPermission('MANAGE_CHANNELS', false, true, true)) {
+      message.reply('**Não tenho permissão para alterar o tópico do canal!** <a:atencao:723878171216314448>')
+      return 0
+    }
+    let text = args.join(' ')
+    if (!text.length) {
+      message.reply('Você esqueceu de colocar a mensagem do tópico! <a:yoeEstrelaBDI:723881976674910262>')
+      return 0
+    } else if (text.length > 1024) {
+      message.reply('O tópico só pode conter 1024 caracteres')
+      return 0
+    }
+    message.channel.setTopic(text).then(() => {
+      message.reply(`Você definiu a mensagem: **${text}** | no tópico do servidor!`)
+    }).catch(()=>{})
+  }
+}
