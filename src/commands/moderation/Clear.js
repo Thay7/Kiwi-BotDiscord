99f89457da2ebfe42eec253262cf102d4f6d7f9a
Thay-3x <thay>
2020-07-03 06:22:12 -0300

    alterações em clear.js

--- src/commands/moderation/Clear.js
+++ src/commands/moderation/Clear.js
@@ -10,15 +10,15 @@ module.exports = class Clear extends Command {
   }
 
   async run({ msg, args, member }) {
-    if(!member.hasPermission('MANAGE_MESSAGES')) return msg.reply('**Você não tem permissão para limpar o chat!** <a:atencao:723878171216314448>')
-    if (!msg.guild.me.hasPermission('MANAGE_MESSAGES')) return msg.reply('**Eu não tenho permissão para apagar mensagens nesse servidor.** <a:AtencaoBDI:723884702271930368>')
+    if(!member.hasPermission('MANAGE_MESSAGES')) return msg.reply('você não tem permissão para limpar o chat!')
+    if (!msg.guild.me.hasPermission('MANAGE_MESSAGES')) return msg.reply('eu não tenho permissão para apagar mensagens nesse servidor!')
     if(!args[0]) return msg.reply('você precisa informar quantas mensagens serão deletadas!')
 
     let quantity = args[0]
 
-    if (isNaN(quantity)) return msg.reply('A quantidade precisa ser um número')
+    if (isNaN(quantity)) return msg.reply('a quantidade precisa ser um número!')
 
-    if (quantity < 1) return msg.reply('O número precisa ser maior que 0')
+    if (quantity < 1) return msg.reply('o número precisa ser maior que 0!')
     else if (quantity > 1000) quantity = 1000
 
     let total = 0
@@ -35,6 +35,6 @@ module.exports = class Clear extends Command {
       quantity -= deletedMessages.size
     }
 
-    msg.channel.send(`<a:BDI_pink_setadireita:723882065346560091> **Removido ${total} mensagens.**`).then(m => m.delete(7000)).catch(() => {})
+    msg.channel.send(`${total} mensagens deletadas! `).then(m => m.delete(7000)).catch(() => {})
   }
 }
