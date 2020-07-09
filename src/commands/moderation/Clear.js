faad5632dee3fc633c2f7508bc52e42a4249ebda
Thay-3x <thay>
2020-07-09 18:53:40 -0300

    clear.js quase pronto

--- src/commands/moderation/Clear.js
+++ src/commands/moderation/Clear.js
@@ -9,23 +9,43 @@ module.exports = class Clear extends Command {
     this.category = 'moderation'
   }
 
-  async run({ msg, args, member, author, channel }) {
+  async run({ msg, args, member, author, channel, mentions,}) {
     const { MessageEmbed } = require('discord.js')
+    const user = args[0] ? mentions.users.first() || await this.client.users.fetch(args[0]) : author
+    
     
     const embed = new MessageEmbed()
-      .setDescription('você não tem permissão para limpar o chat!')
+      .setDescription(`${user}, você não tem permissão para limpar o chat!`)
       .setAuthor(author.tag, author.displayAvatarURL({ format: 'png', dynamic: true, size: 2048 }))
       .setColor('DB7093')
+    if(!member.hasPermission('MANAGE_MESSAGES')) return channel.send(embed).then(m => m.delete({ timeout: 5000 }))
 
-    if(!member.hasPermission('MANAGE_MESSAGES')) return channel.send(embed).then(m => m.delete(7000)).catch(() => {})
-    if (!msg.guild.me.hasPermission('MANAGE_MESSAGES')) return msg.reply('eu não tenho permissão para apagar mensagens nesse servidor!')
-    if(!args[0]) return msg.reply('você precisa informar quantas mensangens serão deletadas!')
+    const embed2 = new MessageEmbed()
+      .setDescription(`${user}, eu não tenho permissão para apagar mensagens nesse servidor!`)
+      .setAuthor(author.tag, author.displayAvatarURL({ format: 'png', dynamic: true, size: 2048 }))
+      .setColor('DB7093')
+    if (!msg.guild.me.hasPermission('MANAGE_MESSAGES'))  channel.send(embed2).then(m => m.delete({ timeout: 5000 }))
+    
+    const embed3 = new MessageEmbed()
+      .setDescription(`${user}, você precisa informar quantas mensangens serão deletadas!`)
+      .setAuthor(author.tag, author.displayAvatarURL({ format: 'png', dynamic: true, size: 2048 }))
+      .setColor('DB7093')
+    if(!args[0]) return channel.send(embed3).then(m => m.delete({ timeout: 5000 }))
+
 
     let quantity = args[0]
 
-    if (isNaN(quantity)) return msg.reply('a quantidade precisa ser um número!')
+    const embed4 = new MessageEmbed()
+      .setDescription(`${user}, a quantidade precisa ser um número!`)
+      .setAuthor(author.tag, author.displayAvatarURL({ format: 'png', dynamic: true, size: 2048 }))
+      .setColor('DB7093')
+    if (isNaN(quantity)) return channel.send(embed4).then(m => m.delete({ timeout: 5000 }))
 
-    if (quantity < 1) return msg.reply('o número precisa ser maior que 0!')
+    const embed5 = new MessageEmbed()
+      .setDescription(`${user}, o número precisa ser maior que 0!`)
+      .setAuthor(author.tag, author.displayAvatarURL({ format: 'png', dynamic: true, size: 2048 }))
+      .setColor('DB7093')
+    if (quantity < 1) return channel.send(embed5).then(m => m.delete({ timeout: 5000 }))
     else if (quantity > 1000) quantity = 1000
 
     let total = 0
@@ -41,7 +61,10 @@ module.exports = class Clear extends Command {
 
       quantity -= deletedMessages.size
     }
-
-    msg.channel.send(`${total} mensagens deletadas! `).then(m => m.delete(7000)).catch(() => {})
+    const embed6 = new MessageEmbed()
+      .setDescription(`${user}, ${total} mensagens deletadas!`)
+      .setAuthor(author.tag, author.displayAvatarURL({ format: 'png', dynamic: true, size: 2048 }))
+      .setColor('DB7093')
+    msg.channel.send(embed6).then(m => m.delete(7000)).catch(() => {})
   }
 }
