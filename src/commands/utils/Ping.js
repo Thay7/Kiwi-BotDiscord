44c0ffa9c12e8b75bec5e7d78406723c0bf90ecf
Thay-3x <thay>
2020-07-09 15:11:01 -0300

    ping.js finalizado

--- src/commands/utils/Ping.js
+++ src/commands/utils/Ping.js
@@ -8,7 +8,21 @@ module.exports = class Ping extends Command {
     this.category = 'utils'
   }
 
-  run({ channel }) {
-    channel.send(`Pong! 🏓 \`${this.client.ws.ping}ms\``)
+  async run({ channel, args, author, member }) {
+    const { MessageEmbed } = require('discord.js')
+    const user = args[0] ? mentions.users.first() || await this.client.users.fetch(args[0]).catch(_ => author) : author
+    const avatar = user.displayAvatarURL({ dynamic: true, size: 2048 })
+    
+    
+    const embed = new MessageEmbed()
+
+      .setTitle('Pong! <:ping:730845575959937025>')
+      .setDescription(`Meu ping atual é: \`${this.client.ws.ping}ms\``)
+      .setAuthor(author.tag, author.displayAvatarURL({ format: 'png', dynamic: true, size: 2048 }))
+      .setColor('DB7093')
+
+
+
+    channel.send(embed)
   }
 }
