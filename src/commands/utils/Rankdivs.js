247541a058b4027ef6b3e89e9403fe7b14c265ca
Thay-3x <thay>
2020-07-10 16:30:05 -0300

    rankdiv.js finalizado

--- src/commands/utils/Rankdivs.js
+++ src/commands/utils/Rankdivs.js
@@ -9,15 +9,26 @@ module.exports = class Divulgadores extends Command {
     this.category = 'utils'
   }
 
-  async run({ channel, msg, client, member, guild, author }) {
+  async run({ channel, msg, client, member, guild, author, args }) {
     const invites = await channel.guild.fetchInvites()
-
-    if (!invites) return msg.reply('<a:dc:723878284517048332> **|** esse servidor não possui convites!')
-    const rank = invites.sort((a, b) => b.uses - a.uses).array().slice(0, 5)
-
-    if (rank.length < 5) return msg.reply(`<a:branco_dccarregando:723881833682567168> **|** ${author}, esse servidor precisa possuir 5 convites para ter um \`ranking\`!`)
+    const user = args[0] ? mentions.users.first() || await this.client.users.fetch(args[0]) : author
 
     const embed = new MessageEmbed()
+      .setDescription(`${user} esse servidor não possui convites!`)
+      .setAuthor(author.tag, author.displayAvatarURL({ format: 'png', dynamic: true, size: 2048 }))
+      .setColor('DB7093')
+      
+    if (!invites) return channel.send(embed).then(m => m.delete({ timeout: 12000 })).catch(() => {})
+    const rank = invites.sort((a, b) => b.uses - a.uses).array().slice(0, 5)
+
+    const embed2 = new MessageEmbed()
+      .setDescription(`${user} esse servidor precisa possuir pelo menos 5 convites para ter um \`ranking\`!`)
+      .setAuthor(author.tag, author.displayAvatarURL({ format: 'png', dynamic: true, size: 2048 }))
+      .setColor('DB7093')
+
+    if (rank.length < 5) return channel.send(embed2).then(m => m.delete({ timeout: 12000 })).catch(() => {})
+
+    const embed3 = new MessageEmbed()
       .setAuthor(`Divulgadores | ${guild.name}`, client.user.avatarURL)
       .setDescription('Esse é meu "ranking" e apenas os melhores na divulgação se encontram nele!')
       .setThumbnail(guild.iconURL)
@@ -29,15 +40,15 @@ module.exports = class Divulgadores extends Command {
     for (const inv of rank) {
       total += inv.uses
       const index = rank.indexOf(inv, 0)
-      embed.addField(`${emojis[index]} **${index + 1}** - ${inv.inviter.username}`, `\`\`\`\nConvidados: ${inv.uses}\`\`\` `)
+      embed3.addField(`${emojis[index]} **${index + 1}** - ${inv.inviter.username}`, `\`\`\`\nConvidados: ${inv.uses}\`\`\` `)
     }
 
-    embed
+    embed3
       .addField('Total Convites:', `<:convites:730852750086111312> ${invites.size}`, true)
       .addField('Total Recrutados:', `<:total:730851292523659295> ${total}`, true)
       .setFooter(msg.guild.name, msg.guild.iconURL({ format: 'png', size: 2048, dynamic: true }))
       .setColor('DB7093')
 
-    channel.send(embed)
+    channel.send(embed3)
   }
 }
