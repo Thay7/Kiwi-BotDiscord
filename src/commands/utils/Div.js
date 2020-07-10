5bc504e5146982879642fe34f9708bd6680981fc
Thay-3x <thay>
2020-07-10 16:37:25 -0300

    div.js quase finalizado

--- src/commands/utils/Div.js
+++ src/commands/utils/Div.js
@@ -10,13 +10,19 @@ module.exports = class Div extends Command {
 
   async run({ guild, channel, author, mentions, args, msg }) {
     const invites = await guild.fetchInvites().catch(_ => null)
-    if (!invites || invites.size === 0) return channel.send(`<a:atencao:723878171216314448> **|** Poing-oing... ${author}, eu não tenho acesso aos convites desse servidor.`)
+    const user = args[0] ? mentions.users.first() || await this.client.users.fetch(args[0]) : author
+    const embed = new MessageEmbed()
+      .setDescription(`${user}, eu não tenho acesso aos convites desse servidor!`)
+      .setAuthor(author.tag, author.displayAvatarURL({ format: 'png', dynamic: true, size: 2048 }))
+      .setColor('DB7093')
 
+    if (!invites || invites.size === 0) return channel.send(embed).then(m => m.delete({ timeout: 12000 })).catch(() => {})
+   
     const guildMember = args[0] ? mentions.users.first() || guild.member(args[0]) : author
 
     const userInvites = invites.filter(i => i.inviter === guildMember)
 
-    const embed = new MessageEmbed()
+    const embed2 = new MessageEmbed()
       .setThumbnail(guild.iconURL({ format: 'png', size: 2048, dynamic: true }))
       .setAuthor('Kyatsu | Divulgadores')
       .addField(`Divulgador: ${guildMember.user ? guildMember.user.tag : guildMember.tag }`, `Convidou um total de **${userInvites.reduce((p, v) => v.uses + p, 0)}** `, true)
@@ -24,6 +30,6 @@ module.exports = class Div extends Command {
       .setFooter(msg.guild.name, msg.guild.iconURL({ format: 'png', size: 2048, dynamic: true }))
       .setColor('DB7093')
       .setAuthor(author.tag, author.displayAvatarURL({ format: 'png', dynamic: true, size: 2048 }))
-    channel.send(embed)
+    channel.send(embed2)
   }
 }
