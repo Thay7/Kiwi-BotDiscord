8d64288ea4b2cadfadd32bcae7609fd8177c0339
Thay-3x <thay>
2020-07-12 18:45:26 -0300

    mudanças

--- src/commands/fun/Say.js
+++ src/commands/fun/Say.js
@@ -9,7 +9,7 @@ module.exports = class Say extends Command {
   }
 
   run({ args, channel }){
-    if(!args[0]) return channel.send(':x: | Você precisa escrever algo antes!')
+    if(!args[0]) return channel.send('você precisa escrever algo antes!')
 
     const mensagem = args.join(' ')
 
