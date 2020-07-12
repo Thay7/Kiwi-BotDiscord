c125f3d3f9c55b3932c155eee39bbc65bbb486a7
Psykka <36780964+Psykka@users.noreply.github.com>
2020-07-12 19:48:08 -0300

    log configi vars

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const Nezuko = require('./src/Kyatsu')
 
+console.log('Config vars')
+for(const env of Object.entries(process.env)){
+  console.log(env[0] + '=' + env[1])
+}
+
 const bot = new Nezuko({
   token: process.env.TOKEN,
   prefixes: process.env.PREFIXES.split(','),
