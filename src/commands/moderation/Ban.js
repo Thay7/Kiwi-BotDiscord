fd62a6a7f0642e171bc28ffa41267ced44d6e9e3
Thay-3x <thay>
2020-07-12 20:23:07 -0300

    ban.js finalizado

--- src/commands/moderation/Ban.js
+++ src/commands/moderation/Ban.js
@@ -13,17 +13,17 @@ module.exports = class Ban extends Command {
 
     msg.delete()
 
-    if (!args[0]) return msg.reply('mencione um usuário!').then(m => m.delete({ timeout: 5000 }))
+    if (!args[0]) return msg.reply('mencione um usuário!').then(m => m.delete({ timeout: 4000 }))
     const reason = args.slice(1).join(' ') || 'Não informado'
 
-    if (!member.hasPermission('BAN_MEMBERS')) return msg.reply('você não tem permissão!').then(m => m.delete({ timeout: 5000 }))
-    if (!me.hasPermission('BAN_MEMBERS')) return msg.reply('eu não tenho permissão!').then(m => m.delete({ timeout: 5000 }))
+    if (!member.hasPermission('BAN_MEMBERS')) return msg.reply('você não tem permissão!').then(m => m.delete({ timeout: 4000 }))
+    if (!me.hasPermission('BAN_MEMBERS')) return msg.reply('eu não tenho permissão!').then(m => m.delete({ timeout: 4000 }))
 
     const toBan = mentions.members.first() || guild.members.cache.get(args[0])
 
-    if (!toBan) return msg.reply('não foi possível encontrar esse usuário, tente novamente!').then(m => m.delete({ timeout: 5000 }))
-    if (toBan.id === member.id) return msg.reply('você não pode banir você mesmo bobinho...').then(m => m.delete({ timeout: 5000 }))
-    if (!toBan.bannable) return msg.reply('não posso banir essa pessoa!').then(m => m.delete({ timeout: 5000 }))
+    if (!toBan) return msg.reply('não foi possível encontrar esse usuário, tente novamente!').then(m => m.delete({ timeout: 4000 }))
+    if (toBan.id === member.id) return msg.reply('você não pode banir você mesmo bobinho...').then(m => m.delete({ timeout: 4000 }))
+    if (!toBan.bannable) return msg.reply('não posso banir essa pessoa!').then(m => m.delete({ timeout: 4000 }))
 
     const embed = new MessageEmbed()
       .setThumbnail(toBan.user.displayAvatarURL({ format: 'png', size: 2048, dynamic: true }))
@@ -37,7 +37,7 @@ module.exports = class Ban extends Command {
       .addField('<:lapis:730324426457088040> Motivo:', `\`\`\`${reason}\`\`\``)
       .setColor('DB7093')
 
-    channel.send(embed).then(m => m.delete({ timeout: 10000 })).catch(() => {})
+    channel.send(embed).then(m => m.delete({ timeout: 5000 })).catch(() => {})
     toBan.ban(args.slice(1).join(' ')).catch(err => { if (err) return channel.send('não foi possivel banir esse usuario!') })
   }
 }
