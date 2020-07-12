2dc56dc3e31097ded5e6c239025443240c406f7e
Psykka <36780964+Psykka@users.noreply.github.com>
2020-07-12 20:01:05 -0300

    fix

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const bot = new Nezuko({
   token: process.env.TOKEN,
   prefixes: process.env.PREFIXES.split(','),
   owners: process.env.OWNERS.split(','),
-  presence: process.env.PRESENCE || null
+  presence: JSON.parse(process.env.PRESENCE) || null
 })
 
 bot.launch()
