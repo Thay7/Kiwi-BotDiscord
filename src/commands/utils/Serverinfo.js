3dfa9257b07857496057ded8bb8882003196b3c4
Thay França <thay3x@gmail.com>
2020-08-23 13:35:01 -0300

    svinfo

---
+++ src/commands/utils/Serverinfo.js
@@ -0,0 +1,55 @@
+const Command = require('../../lib/strucutures/Command')
+const { MessageEmbed } = require('discord.js')
+const moment = require('moment')
+moment.locale('pt-BR')
+
+module.exports = class Serverinfo extends Command {
+  constructor(client) {
+    super(client)
+    this.name = 'serverinfo'
+    this.aliases = ['svinfo']
+    this.category = 'utils'
+  }
+
+  async run({ guild, members, channel, msg}) {
+    const embed = new MessageEmbed()
+      .setTitle('<:ajuda:734901641408675853> Informações do Servidor')
+      .setColor('DB7093')
+      .setThumbnail(guild.iconURL)
+      .addFields([
+        {
+          name: 'Nome',
+          value: `\`${guild.name}\``,
+          inline: true
+        },
+        {
+          name: 'ID',
+          value: `\`${guild.id}\``,
+          inline: true
+        },
+        {
+          name: 'Membros:',
+          value: `\`${guild.memberCount}\``,
+          inline: true
+        },
+        {
+          name: 'Criado em:',
+          value: moment(guild.createdAt).format('LLL'),
+          inline: true
+        },
+        {
+          name: 'Entrei em:',
+          value: moment(guild.joinedTimestamp).format('LLL'),
+          inline: true
+        },
+        {
+          name: 'Dono:',
+          value: `${guild.owner}`,
+          inline: true
+        },
+      ])
+      .setFooter(msg.guild.name, msg.guild.iconURL({ format: 'png', size: 2048, dynamic: true }))
+      
+    channel.send(embed)
+  }                                                         
+}
