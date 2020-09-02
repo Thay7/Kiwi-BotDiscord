524282b955df0daa121b49133addde59db95f72e
Thay França <thay3x@gmail.com>
2020-09-02 13:04:09 -0300

    div

--- src/commands/moderation/Div.js
+++ src/commands/moderation/Div.js
@@ -23,10 +23,8 @@ module.exports = class Div extends Command {
     const userInvites = invites.filter(i => i.inviter === guildMember)
 
     const embed2 = new MessageEmbed()
-      .setThumbnail(guild.iconURL({ format: 'png', size: 2048, dynamic: true }))
-      .addField(`<:user_kiwi:748131837414735982> Divulgador: ${guildMember.user ? guildMember.user.tag : guildMember.tag }`, `Convidou um total de: **${userInvites.reduce((p, v) => v.uses + p, 0)}** `, true)
-      .addField('<:convites_kiwi:748547923620659350> Convites:', `${ userInvites.size > 0 ?  userInvites.reduce((a, { code, uses }) => a + `https://discord.gg/${code} -  **${uses} membros**\n`, '') : `**${guildMember}** não possui convites no servidor.`}`, false)
-      .setFooter(msg.guild.name, msg.guild.iconURL({ format: 'png', size: 2048, dynamic: true }))
+      .addField(`Divulgador: ${guildMember.user ? guildMember.user.tag : guildMember.tag }`, `Convidou um total de: **${userInvites.reduce((p, v) => v.uses + p, 0)}** membros `, true)
+      .addField('Convites:', `${ userInvites.size > 0 ?  userInvites.reduce((a, { code, uses }) => a + `https://discord.gg/${code}\n`, '') : `**${guildMember}** não possui convites no servidor.`}`, false)
       .setColor('DB7093')
     channel.send(embed2)
   }
