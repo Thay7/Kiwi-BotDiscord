650e393e130c639db5554cddd30b1e93c099dd31
Thay França <thay3x@gmail.com>
2020-12-19 19:11:39 -0300

    a

--- src/commands/utils/Comandos.js
+++ src/commands/utils/Comandos.js
@@ -20,13 +20,13 @@ module.exports = class Comandos extends Command {
       .setColor('DB7093')
       .setFooter(author.tag, author.displayAvatarURL({ format: 'png', dynamic: true, size: 2048 }))
         
-    const ADM = new MessageEmbed()
+    const MOD = new MessageEmbed()
       .setTitle('<:config_kiwi:748131837003431936> Moderação')
       .setDescription('\`Ban/Unban\` - Bane/Desbane um membro  \n\n\`Clear\` - Apaga uma quantidade específica de mensagens de um chat \n\n\`Div\` - Mostra quantos membros foram recrutados e os convites do recrutador \n\n\`Rankdivs\` - Mostra o ranking de divulgações do servidor \n\n\`Mute/Unmute\` - Muta/Desmuta um membro \n\n\`Tópico\` - Define a mensagem do topico de um canal')
       .setColor('DB7093')
       .setFooter(author.tag, author.displayAvatarURL({ format: 'png', dynamic: true, size: 2048 }))
         
-    const FUN = new MessageEmbed()
+    const DIV = new MessageEmbed()
       .setTitle('<:game_kiwi:748134067878232074> Diversão')
       .setDescription('\`Avatar\` - Mostra o avatar do usuário \n\n\`Say\` - O bot replica sua mensagem \n\n\`Hug\` - Dá um abraço no usuário mencionado \n\n\`Kiss\` - Dá um beijo no usuário mencionado \n\n\`Slap\` - Dá um tapa no usuário mencionado')
       .setColor('DB7093')
@@ -44,13 +44,13 @@ module.exports = class Comandos extends Command {
         msg2.react('748567313074225282')
 
         const comandsFilter = (reaction, user) => reaction.emoji.name === 'next_kiwi' && user.id === msg.author.id
-        const admFilter = (reaction, user) => reaction.emoji.name === 'config_kiwi' && user.id === msg.author.id
-        const funFilter = (reaction, user) => reaction.emoji.name === 'game_kiwi' && user.id === msg.author.id
+        const modFilter = (reaction, user) => reaction.emoji.name === 'config_kiwi' && user.id === msg.author.id
+        const divFilter = (reaction, user) => reaction.emoji.name === 'game_kiwi' && user.id === msg.author.id
         const ultilsFilter = (reaction, user) => reaction.emoji.name === 'lupa_kiwi' && user.id === msg.author.id
 
         const comands = msg2.createReactionCollector(comandsFilter, { time: 100000 })
-        const adm = msg2.createReactionCollector(admFilter, { time: 100000 })
-        const fun = msg2.createReactionCollector(funFilter, { time: 100000 })
+        const mod = msg2.createReactionCollector(modFilter, { time: 100000 })
+        const div = msg2.createReactionCollector(divFilter, { time: 100000 })
         const ultils = msg2.createReactionCollector(ultilsFilter, { time: 100000 })
 
       
@@ -59,12 +59,12 @@ module.exports = class Comandos extends Command {
           page--
           msg2.edit(COMANDS)
         })
-        adm.on('collect', r => {
+        mod.on('collect', r => {
           if (page === 2) return
           page++
           msg2.edit(ADM)
         })
-        fun.on('collect', r => {
+        div.on('collect', r => {
           if (page === 3) return
           page++
           msg2.edit(FUN)
