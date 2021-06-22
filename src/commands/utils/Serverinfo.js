171b6244f8ac0748d0be343d1275d641c9a5c3bd
Thay <67573675+Thay7@users.noreply.github.com>
2021-06-22 20:50:45 -0300

    Update Serverinfo.js

--- src/commands/utils/Serverinfo.js
+++ src/commands/utils/Serverinfo.js
@@ -13,37 +13,37 @@ module.exports = class Serverinfo extends Command {
 
   async run({ guild, member, author, channel, msg}) {
     const embed = new MessageEmbed()
-      .setTitle('<:info_kiwi:748230688481869905> Informações do Servidor')
+      .setTitle('Informações do Servidor')
       .setColor('DB7093')
       .setThumbnail(guild.iconURL)
       .addFields([
         {
-          name: '<:server_kiwi:748230688238600243> Nome',
+          name: 'Nome',
           value: `\`${guild.name}\``,
           inline: true
         },
         {
-          name: '<:info__kiwi:748131836496052266> ID',
+          name: 'ID',
           value: `\`${guild.id}\``,
           inline: true
         },
         {
-          name: '<:members_kiwi:748230688200982614> Membros:',
+          name: 'Membros:',
           value: `\`${guild.memberCount}\``,
           inline: true
         },
         {
-          name: '<:conta__kiwi:748131836609429525> Criado em:',
+          name: 'Criado em:',
           value: moment(guild.createdAt).format('LLL'),
           inline: true
         },
         {
-          name: '<:entrou_kiwi:748131836785328211> Entrei em:',
+          name: 'Entrei em:',
           value: moment(member.joinedAt).format('LLL'),
           inline: true
         },
         {
-          name: '<:chave_kiwi:748232441440763904> Dono:',
+          name: 'Dono:',
           value: `${guild.owner}`,
           inline: true
         },
