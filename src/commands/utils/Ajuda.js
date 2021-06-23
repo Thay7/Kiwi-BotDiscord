3753ec8cfb9bfbe221f18ee6e286b3960db2679e
Thay <67573675+Thay7@users.noreply.github.com>
2021-06-22 21:03:00 -0300

    Update Ajuda.js

--- src/commands/utils/Ajuda.js
+++ src/commands/utils/Ajuda.js
@@ -14,7 +14,7 @@ module.exports = class Ajuda extends Command {
     const embed = new MessageEmbed()
       .setThumbnail(client.user.displayAvatarURL({ format: 'png', size: 2048, dynamic: true }))
       .setTitle('Ajuda')
-      .setDescription('**Olá, eu sou a Kiwi!** \n\n\`Fui desenvolvida com o intuito de te auxiliar na moderação do seu servidor. Aqui estão algumas informações importantes sobre mim:\` \n\ **Prefixo:**\`k!\` \n\n **Comandos:** \`k!comandos\` \n\n**Convite:** \`k!convite\`')
+      .setDescription('**Olá, eu sou a Kiwi!** \n\n\`Fui desenvolvida com o intuito de te auxiliar na moderação do seu servidor. Aqui estão algumas informações importantes sobre mim:\` \n\n**Prefixo:**\`k!\` \n\n**Comandos:** \`k!comandos\` \n\n**Convite:** \`k!convite\`')
       .setColor('DB7093')
     channel.send(embed)
   }
