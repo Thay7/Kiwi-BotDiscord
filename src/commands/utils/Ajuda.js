cad4023f43e23d367b0506a8f788aefcbe64f7a7
Thay <67573675+Thay7@users.noreply.github.com>
2021-06-22 21:01:11 -0300

    Update Ajuda.js

--- src/commands/utils/Ajuda.js
+++ src/commands/utils/Ajuda.js
@@ -13,8 +13,8 @@ module.exports = class Ajuda extends Command {
 
     const embed = new MessageEmbed()
       .setThumbnail(client.user.displayAvatarURL({ format: 'png', size: 2048, dynamic: true }))
-      .setTitle('<:info_kiwi:748230688481869905> Ajuda')
-      .setDescription('**Olá, eu sou a Kiwi!** <:kiwi_kiwi:748567313309106196> \n\n<:lupa_kiwi:748567313074225282> \`Fui desenvolvida com o intuito de te auxiliar na moderação do seu servidor. Aqui estão algumas informações importantes sobre mim:\` \n\n<:prefix_kiwi:748567313057579138> **Prefixo:**\`k!\` \n\n<:comandos_kiwi:748568323721003010> **Comandos:** \`k!comandos\` \n\n<:convites_kiwi:748547923620659350> **Convite:** \`k!convite\`')
+      .setTitle('Ajuda')
+      .setDescription('**Olá, eu sou a Kiwi!** \n\n\`Fui desenvolvida com o intuito de te auxiliar na moderação do seu servidor. Aqui estão algumas informações importantes sobre mim:\` \n\ **Prefixo:**\`k!\` \n\n **Comandos:** \`k!comandos\` \n\n**Convite:** \`k!convite\`')
       .setColor('DB7093')
     channel.send(embed)
   }
