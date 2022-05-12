85cc64cd0f491262fa5001debd826840b7350f54
Thay <thay3x@gmail.com>
2022-05-12 08:35:41 -0300

    teste

--- src/commands/utils/Ajuda.js
+++ src/commands/utils/Ajuda.js
@@ -14,7 +14,7 @@ module.exports = class Ajuda extends Command {
     const embed = new MessageEmbed()
       .setThumbnail(client.user.displayAvatarURL({ format: 'png', size: 2048, dynamic: true }))
       .setTitle('<:ajuda:734901641408675853> Ajuda')
-      .setDescription('**Olá, eu sou a Kiwi!** <:kyatsu:735553174341157025> \n\n<:lupa:735555823547187290> \`Fui desenvolvida com o intuito de te auxiliar na moderação do seu servidor. Aqui estão algumas informações importantes sobre mim:\` \n\n<:prefixo:735303901825204286> **Prefixo:**\`k!\` \n\n<:comandos:735306352296984586> **Comandos:** \`k!comandos\` \n\n<:convites:730852750086111312> **Convite:** \`k!convite\`')
+      .setDescription('**Olá, eu sou a Kiwi! Isso é um teste!** <:kyatsu:735553174341157025> \n\n<:lupa:735555823547187290> \`Fui desenvolvida com o intuito de te auxiliar na moderação do seu servidor. Aqui estão algumas informações importantes sobre mim:\` \n\n<:prefixo:735303901825204286> **Prefixo:**\`k!\` \n\n<:comandos:735306352296984586> **Comandos:** \`k!comandos\` \n\n<:convites:730852750086111312> **Convite:** \`k!convite\`')
       .setColor('25EC91')
       .setFooter(author.tag, author.displayAvatarURL({ format: 'png', dynamic: true, size: 2048 }))
       
