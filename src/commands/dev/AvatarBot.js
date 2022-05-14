d005ddc5185d4b850e16ee8c71e376c79f6f7741
Thay <thay3x@gmail.com>
2022-05-13 22:59:49 -0300

    .

--- src/commands/dev/AvatarBot.js
+++ src/commands/dev/AvatarBot.js
@@ -10,8 +10,11 @@ module.exports = class Say extends Command {
   }
 
   run({ client, args, author, channel }) {
+    if(!['726509292659671070'].includes(message.author.id))
+      return
     if (!args[0])
       return channel.send(`${author} insira um link!`)
+
     client.user.setAvatar(args[0])
     channel.send(`${author} minha nova foto estilosa ficou demais!`)
   }
