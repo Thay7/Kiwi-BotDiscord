26919a8dcc0a473a00c3b18c0b3dd047b884120d
Thay <thay3x@gmail.com>
2022-06-11 13:49:39 -0300

    add emoji help

--- src/commands/utils/Ajuda.js
+++ src/commands/utils/Ajuda.js
@@ -13,7 +13,7 @@ module.exports = class Ajuda extends Command {
 
     const embed = new MessageEmbed()
       .setThumbnail(client.user.displayAvatarURL({ format: 'png', size: 2048, dynamic: true }))
-      .setTitle('Ajuda')
+      .setTitle('<:ajuda:984978051043590205> Ajuda')
       .setDescription('**Olá, eu sou a Kiwi!** \n\n\`Fui desenvolvida com o intuito de te auxiliar na moderação do seu servidor. Aqui estão algumas informações importantes sobre mim:\` \n\n <:prefixo:857252134196871238> **Prefixo:**\`k!\` \n\n <:comandos:857252199046316042> **Comandos:** \`k!comandos\` \n\n <:convites:857243714651684865> **Convite:** \`k!convite\`')
       .setColor('DB7093')
     channel.send(embed)
