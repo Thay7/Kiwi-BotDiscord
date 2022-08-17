e50875257d77fd7d03f4da0fc5a422dbef5c8ab9
Thay7 <thay3x@gmail.com>
2022-08-16 21:30:46 -0300

    add owner

---
+++ src/commands/fun/Owner.js
@@ -0,0 +1,23 @@
+const Command = require('../../lib/strucutures/Command')
+const { MessageEmbed } = require('discord.js')
+const neko = new (require('nekos.life'))
+
+module.exports = class Owner extends Command {
+    constructor(client){
+      super(client)
+      this.name = 'owner'
+      this.aliases = ['dono']
+      this.category = 'fun'
+    }
+
+    run({ args, channel, author, msg}){
+       
+
+        const embed = new MessageEmbed()
+        .setDescription(`<:insta:857243048799109120> **Avatar de** ${user} \n<:download:984978058064846879> Clique [aqui](${avatar}) para baixar`)
+        .setImage(avatar)
+        .setColor('DB7093')
+        .setFooter(`Autor: ${author.tag}`)
+      channel.send(embed)
+    }
+  }
