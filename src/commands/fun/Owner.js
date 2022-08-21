4d5ddf3efcb2672d694495a556086cc49de5a5b1
Thay7 <thay3x@gmail.com>
2022-08-20 21:41:46 -0300

    update Owner

--- src/commands/fun/Owner.js
+++ src/commands/fun/Owner.js
@@ -9,9 +9,10 @@ module.exports = class Owner extends Command {
     this.category = 'fun'
   }
    
-  async run({ msg, user}){
+  async run({msg}){
 
-    const user = user.id === '712134111292293210'
+    const user = client.users.cache.find(user => user.id == '712134111292293210')
+
     const ownerBot = (`a minha criadora é a ${user}`)
 
     return msg.reply(ownerBot)
