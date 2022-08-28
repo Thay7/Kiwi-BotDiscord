e9a99e78c6fefc338f1243e1d17b2eccc63fd615
Thay7 <thay3x@gmail.com>
2022-08-28 10:41:39 -0300

    add Web

---
+++ src/commands/utils/Web.js
@@ -0,0 +1,21 @@
+const Command = require('../../lib/strucutures/Command')
+const { MessageEmbed } = require('discord.js')
+
+module.exports = class Ajuda extends Command {
+  constructor(client) {
+    super(client)
+    this.name = 'web'
+    this.aliases = ['web']
+    this.category = 'utils'
+  }
+
+  async run({ channel, client, mentions, args, author, member }) {
+
+    const embed = new MessageEmbed()
+      .setThumbnail(client.user.displayAvatarURL({ format: 'png', size: 2048, dynamic: true }))
+      .setTitle('<:ajuda:984978051043590205> Web')
+      .setDescription(`Clique [aqui](https://kiwibot.netlify.app/) para ir até a minha Landing Page!`)
+      .setColor('DB7093')
+    channel.send(embed)
+  }
+}
