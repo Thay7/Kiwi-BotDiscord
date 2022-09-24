654c4be64058220a20b915c8bd8037e283176b85
Thay7 <thay3x@gmail.com>
2022-09-24 17:52:12 -0300

    updates

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const Nezuko = require('./src/Kyatsu')
-
+const Kiwi = require('./src/Kyatsu')
+const { GatewayIntentBits } = require('discord.js')
 
 console.log('Config vars')
 for(const env of Object.entries(process.env)){
   console.log(env[0] + '=' + env[1])
 }
 
-const bot = new Nezuko({
+const bot = new Kiwi({
   token: process.env.TOKEN,
   prefixes: process.env.PREFIXES.split(','),
   owners: process.env.OWNERS.split(','),
-  presence: JSON.parse(process.env.PRESENCE) || null
+  presence: JSON.parse(process.env.PRESENCE) || null,
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits]
 })
 
 bot.launch()
