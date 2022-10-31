743d5c2731ec2461820f832ee62c2a4e3315fa00
Thay7 <thay3x@gmail.com>
2022-10-30 21:50:44 -0300

    update

---
+++ src/commands/dev/guilds.js
@@ -0,0 +1,22 @@
+/* eslint-disable quotes */
+const Discord = require('discord.js')
+const Command = require('../../lib/strucutures/Command')
+
+module.exports = class guilds extends Command {
+  constructor(client) {
+    super(client)
+    this.name = 'guilds'
+    this.aliases = ['servers']
+    this.category = 'utils'
+    this.hidden = true
+  }
+
+  async run ({members, client, size, channel}) {
+      
+    const guildArray = client.guilds.cache.map (g => `${`Nome: \`${g.name}\``}⠀${`Membros: \`${g.members.cache.size}\``}`) 
+    
+    channel.send(`${guildArray.join("\n")}`)
+    
+    
+  }
+}
