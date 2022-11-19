6f12d755eaea26244ea4df46a2cb5b9bfa289c1f
Thay7 <thay3x@gmail.com>
2022-11-19 13:40:17 -0300

    update owner

--- src/commands/utils/Owner.js
+++ src/commands/utils/Owner.js
@@ -12,17 +12,7 @@ module.exports = class Owner extends Command {
   async run({ msg, client, user }) {
 
     const ownerBot = client.users.cache.find(user => user.id == '712134111292293210')
-    
-    if (!!ownerBot) {
 
-      return msg.reply(`a minha criadora é a ${ownerBot}!`)
-
-    } else {
-      
-      return msg.reply(`a minha criadora é a Thay#8579!`)
-      
-    }
-
-
+    return msg.reply(`a minha criadora é a ${ownerBot}!`)
   }
 }
