0073df3b058e7d963022d49b258f400d6a38a5c8
Thay7 <thay3x@gmail.com>
2022-11-19 13:47:29 -0300

    update owner

--- src/commands/utils/Owner.js
+++ src/commands/utils/Owner.js
@@ -13,8 +13,12 @@ module.exports = class Owner extends Command {
 
     const ownerBot = client.users.cache.find(user => user.id == '712134111292293210')
 
+    if (!!ownerBot) {
+     msg.reply(`a minha criadora é a ${ownerBot}!`)
+  
+    } else {
+      return msg.reply(`a minha criadora é a bla bla bla}!`)
 
-    return ownerBot
-
+    }
   }
 }
